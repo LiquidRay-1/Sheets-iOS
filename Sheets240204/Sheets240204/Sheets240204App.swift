8b715eb722eccb6a1c515461ee716b702cbf27ae
Iván Tortosa <152330066+LiquidRay-1@users.noreply.github.com>
2024-03-04 19:50:53 +0100

    Add files via upload

---
+++ Sheets240204/Sheets240204/Sheets240204App.swift
@@ -0,0 +1,17 @@
+//
+//  Sheets240204App.swift
+//  Sheets240204
+//
+//  Created by dam2 on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Sheets240204App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
